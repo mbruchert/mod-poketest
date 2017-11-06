7bd5a4893eb37d631d68e06bd1812909fba06513
mbb <mbb-mail@gmx.de>
2017-11-06 21:09:34 +0100

    Add currently useless pokeball, superball and hyperball

---
+++ pokeitems/init.lua
@@ -0,0 +1,14 @@
+core.register_craftitem("pokeitems:pokeball", {
+	description = "pokeball",
+	inventory_image = "pokeitems_bokeball.png",
+})
+
+core.register_craftitem("pokeitems:superball", {
+	description = "superball",
+	inventory_image = "pokeitems_superball.png",
+})
+
+core.register_craftitem("pokeitems:hyperball", {
+	description = "hyperball",
+	inventory_image = "pokeitems_hyperball.png",
+})
